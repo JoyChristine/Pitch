458a570f7f3e0945b08841a9ea8ba53d06447109
Joy Christine <joychristin2@gmail.com>
2022-05-08 20:31:53 +0300

    add classes & import os

--- config.py
+++ config.py
@@ -2,28 +2,23 @@ import os
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nduta:0000@localhost/watchlist'
-
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://pitch:mypass@localhost/work'
     SECRET_KEY = 'Secretkey'
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = 'devpythonflask@gmail.com'
-    MAIL_PASSWORD = '@whysoserious?'
     
 
 
 class ProdConfig(Config):
     pass
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
-    # if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-    #     SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
         
 
 class DevConfig(Config):
+    
     DEBUG = True
 
+
+# class TestCase(Config):
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jk:0000@localhost/pitch'
+#     DEBUG = True
 
 config_options = {
 'development':DevConfig,
