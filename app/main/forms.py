f438768c8d4b19763d4955ec4de5bd102089be98
Joy Christine <joychristin2@gmail.com>
2022-05-10 09:06:05 +0300

    disable csrf , add lass config

--- app/main/forms.py
+++
@@ -1,8 +0,0 @@
-from wtforms import TextAreaField,SubmitField
-from wtforms.validators import DataRequired
-from flask_wtf import FlaskForm
-
-
-class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
-    submit = SubmitField('Submit')
--- config.py
+++ config.py
@@ -3,8 +3,17 @@ import os
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://pitch:mypass@localhost/work'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY = 'Secretkey'
-    
+    SESSION_COOKIE_SECURE = False
+    # email configs
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("devpythonflask@gmail.com")
+    MAIL_PASSWORD = os.environ.get("@whysoserious?")
+   
+    WTF_CSRF_ENABLED = False
 
 
 class ProdConfig(Config):
@@ -12,7 +21,6 @@ class ProdConfig(Config):
         
 
 class DevConfig(Config):
-    
     DEBUG = True
 
 
