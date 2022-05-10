0e7fee8777f6071d5f7365107158d4d1445b15e7
Joy Christine <joychristin2@gmail.com>
2022-05-10 09:44:10 +0300

    add pitch,comment & update forms

---
+++ app/main/forms.py
@@ -0,0 +1,19 @@
+from wtforms import TextAreaField,SubmitField
+from wtforms.validators import InputRequired
+from flask_wtf import FlaskForm
+
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Write something about yourself',validators = [InputRequired()])
+    submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    pitch_title = TextAreaField('Pitch title',validators = [InputRequired()])
+    content = TextAreaField('Pitch content',validators = [InputRequired()])
+    category = TextAreaField('Pitch category',  validators=[InputRequired()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Any Comment',validators = [InputRequired()])
+    submit = SubmitField('Submit')
