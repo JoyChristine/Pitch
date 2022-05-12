3d5e17513b7503dca54f51b9b021211afc8e957c
Joy Christine <joychristin2@gmail.com>
2022-05-12 09:36:53 +0300

    add prodconfig class

--- config.py
+++ config.py
@@ -18,8 +18,10 @@ class Config:
 
 
 class ProdConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://", 1) 
-    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",)
+
  
 class DevConfig(Config):
     DEBUG = True
