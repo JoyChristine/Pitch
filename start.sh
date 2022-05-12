2b7ab7b7e02c05005a7bce12fd5d6aa727698b11
Joy Christine <joychristin2@gmail.com>
2022-05-12 13:37:19 +0300

    deploy

--- start.sh
+++
@@ -1,4 +0,0 @@
-export MAIL_USERNAME = 'devpythonflask@gmail.com'
-export MAIL_PASSWORD = "@whysoserious?"
-export SECRET_KEY = 'SecretKey'
-export DATABASE_URL = 'postgresql+psycopg2://pitch:mypass@localhost/work'
